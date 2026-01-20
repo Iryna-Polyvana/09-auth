0d22de1cc7aa3e76a5201d24bd03e009fa646882
Iryna Polyvana <irynapolyvana@gmail.com>
2026-01-20 12:10:23 +0100

    fix: prevent redirect to home after login when navigating to notes

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,7 +1,10 @@
+'use client'
+import { useAuthStore } from '@/lib/store/authStore';
 import AuthNavigation from '../AuthNavigation/AuthNavigation';
 import css from './Header.module.css';
 import Link from 'next/link';
 const Header = () => {
+  const {isAuthenticated} = useAuthStore();
   return (
     <header className={css.header}>
       <Link href="/" aria-label="Home">
@@ -14,11 +17,13 @@ const Header = () => {
               Home
             </Link>
           </li>
-          <li>
+          {isAuthenticated && (
+            <li>
             <Link className={css.headerLink} href="/notes/filter/all">
               Notes
             </Link>
           </li>
+          )}
           <AuthNavigation />
         </ul>
       </nav>
